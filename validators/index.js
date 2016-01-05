899639a41be6e459f5cccba5e24f630ba43da45d
Andreas Søvik <andreas.sovik@vg.no>
2016-01-05 14:17:20 +0100

    Update readme, package name and docs for cli move

--- validators/index.js
+++ validators/index.js
@@ -1,5 +1,5 @@
 /**
 * This file makes it easier to import the validators.
-* Can use "roc-config/validators" to only get the validatiors.
+* Can use "roc/validators" to only get the validatiors.
 */
 module.exports = require('../lib/validation/validators');
