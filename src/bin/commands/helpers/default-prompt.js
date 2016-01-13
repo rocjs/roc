fc31083d444b1bbb62a76839e5de2718428d788f
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-01-13 10:38:15 +0100

    Changed to using a Handlebars syntax

--- src/bin/commands/helpers/default-prompt.js
+++ src/bin/commands/helpers/default-prompt.js
@@ -1,27 +1,27 @@
 import 'source-map-support/register';
 
 /**
- * The default prompot options.
+ * The default prompt options.
  */
 export const prompt = [{
     type: 'input',
-    name: 'appName',
+    name: 'rocAppName',
     message: 'What\'s the name of your application?',
     default: 'my-roc-app',
     filter: (input) => input.toLowerCase().split(' ').join('-')
 }, {
     type: 'input',
-    name: 'appDesc',
+    name: 'rocAppDesc',
     message: 'What\'s the description for the application?',
     default: 'My Roc Application'
 }, {
     type: 'input',
-    name: 'appAuthor',
+    name: 'rocAppAuthor',
     message: 'Who\'s the author of the application?',
     default: 'John Doe'
 }, {
     type: 'input',
-    name: 'appLicense',
+    name: 'rocAppLicense',
     message: 'What\'s the license for the application?',
     default: 'MIT'
 }];
