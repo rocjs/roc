509b9fd63a79ff73f5d499586623046617e44a57
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-01-14 14:07:54 +0100

    Cleanup in the tests

--- test/bin/helpers/general.js
+++ test/bin/helpers/general.js
@@ -1,34 +1,37 @@
-import chai from 'chai';
+import expect from 'expect';
 import path from 'path';
-chai.should();
 
-describe('helpers', () => {
-    describe('general', () => {
-        const general = require('../../../src/bin/commands/helpers/general');
+describe('roc', () => {
+    describe('bin', () => {
+        describe('helpers', () => {
+            describe('general', () => {
+                const general = require('../../../src/bin/commands/helpers/general');
 
-        describe('validRocProject', () => {
-            it('should return false if package.json does not exist', () => {
-                const invalidPath = path.join(__dirname, '/invalidpath');
-                const result = general.validRocProject(invalidPath);
-                result.should.be.equal(false);
-            });
+                describe('validRocProject', () => {
+                    it('should return false if package.json does not exist', () => {
+                        const invalidPath = path.join(__dirname, '/invalidpath');
+                        const result = general.validRocProject(invalidPath);
+                        expect(result).toBe(false);
+                    });
 
-            it('should return false if config and extension is missing', () => {
-                const validPath = path.join(__dirname, '/data/package/invalid');
-                const result = general.validRocProject(validPath);
-                result.should.be.equal(false);
-            });
+                    it('should return false if config and extension is missing', () => {
+                        const validPath = path.join(__dirname, '/data/package/invalid');
+                        const result = general.validRocProject(validPath);
+                        expect(result).toBe(false);
+                    });
 
-            it('should return true if package.json exists with roc config', () => {
-                const validPath = path.join(__dirname, '/data/package/valid-config');
-                const result = general.validRocProject(validPath);
-                result.should.be.equal(true);
-            });
+                    it('should return true if package.json exists with roc config', () => {
+                        const validPath = path.join(__dirname, '/data/package/valid-config');
+                        const result = general.validRocProject(validPath);
+                        expect(result).toBe(true);
+                    });
 
-            it('should return true if package.json exists with roc extension', () => {
-                const validPath = path.join(__dirname, '/data/package/valid');
-                const result = general.validRocProject(validPath);
-                result.should.be.equal(true);
+                    it('should return true if package.json exists with roc extension', () => {
+                        const validPath = path.join(__dirname, '/data/package/valid');
+                        const result = general.validRocProject(validPath);
+                        expect(result).toBe(true);
+                    });
+                });
             });
         });
     });
