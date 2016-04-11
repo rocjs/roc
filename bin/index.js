db4905453f0b42c3d0e647321d67bc42f09a392b
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-04-11 09:38:31 +0200

    Removed unnecessary return

--- bin/index.js
+++ bin/index.js
@@ -15,16 +15,16 @@ if (currentCli !== localCli) {
     // Are we global?
     if (!fileExists(path.resolve(process.cwd(), 'node_modules/.bin/roc'))) {
         updateNotifier({ pkg }).notify({ defer: false });
-        return require(currentCli);
-    }
-
-    // Will try to use the local CLI over the global one
-    try {
-        const cli = require.resolve(localCli);
-        console.log('Found a local version of Roc, will use that over the global one.', '\n');
-        return require(cli);
-    } catch (err) {
-        console.log('An error happened when trying to load local CLI, will use the global one.', '\n');
+        require(currentCli);
+    } else {
+        // Will try to use the local CLI over the global one
+        try {
+            const cli = require.resolve(localCli);
+            console.log('Found a local version of Roc, will use that over the global one.', '\n');
+            return require(cli);
+        } catch (err) {
+            console.log('An error happened when trying to load local CLI, will use the global one.', '\n');
+        }
     }
 }
 
