e166f27aaa718ad1cd53fc12f3b31fa75a28acfe
bjarneo <bjarne.overli@vg.no>
2016-05-11 10:32:35 +0200

    add generic convert function

--- src/converters/index.js
+++ src/converters/index.js
@@ -3,4 +3,5 @@ export toRegExp from './to-regexp';
 export toBoolean from './to-boolean';
 export toInteger from './to-integer';
 export toObject from './to-object';
+export convert from './convert';
 export automatic from './automatic';
