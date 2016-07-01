75bbddb73679aee47c934f2c72022bb7cacdecf6
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-07-01 13:20:46 +0200

    Improved the runtime slightly
    Now it can take in a file that will be required after initialisation has been completed.

--- runtime/index.js
+++ runtime/index.js
@@ -1,1 +1,1 @@
-module.exports = require('../lib/runtime');
+module.exports = require('../lib/runtime').default;
--- src/runtime/index.js
+++ src/runtime/index.js
@@ -1,22 +1,38 @@
+import { join } from 'path';
+
+import { isString } from 'lodash';
+
 import initContext from '../context/initContext';
 import { appendConfig } from '../configuration/manageConfig';
 import runHook from '../hooks/runHook';
 import { appendSettings } from '../configuration/manageSettings';
 
-export default function initRuntime({
-    verbose = false,
-    directory = process.cwd(),
-    projectConfigPath
-}) {
-    const { config } = initContext({
+export default function initRuntime(filenameOrOptions, options) {
+    const {
+        file,
+        opts: {
+            verbose = false,
+            directory = process.cwd(),
+            projectConfigPath
+        } = {}
+    } = isString(filenameOrOptions) ?
+        ({ file: filenameOrOptions, opts: options }) :
+        ({ opts: filenameOrOptions });
+
+    return initContext({
         verbose,
         directory,
         projectConfigPath
+    }).then(({ config }) => {
+        runHook('roc')('update-settings', () => context.config.settings)(
+            (newSettings) => context.config.settings = appendSettings(newSettings, context.config)
+        );
+
+        appendConfig(config);
+    }).then(() => {
+        // If a filename has been provided we will try to load it
+        if (file) {
+            require(join(directory, file));
+        }
     });
-
-    runHook('roc')('update-settings', () => context.config.settings)(
-        (newSettings) => context.config.settings = appendSettings(newSettings, context.config)
-    );
-
-    appendConfig(config);
 }
