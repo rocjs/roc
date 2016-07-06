087a1f5b2962738b7bb634b01d9509911bc6732d
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-07-06 16:04:10 +0200

    Removed the special handling for the runtime group in settings
    This means that user will now be required to write out the complete path to a setting even for runtime settings.

--- src/documentation/helpers/toCliOption.js
+++ src/documentation/helpers/toCliOption.js
@@ -6,10 +6,5 @@
  * @returns {string} - The cli option to set the given configuration option.
  */
 export default function toCliOption(configPaths) {
-    // Runtime should be added directly
-    const paths = configPaths[0] === 'runtime' ?
-        configPaths.slice(1) :
-        configPaths;
-
-    return '--' + paths.join('-');
+    return '--' + configPaths.join('-');
 }
