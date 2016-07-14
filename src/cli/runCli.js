6f72950771dc82d68879d48f3b9aea4559a85829
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-07-14 20:53:03 +0200

    Updated runCli to use the now synchronous initContext
    Changed from appendConfig to setConfig to fix a bug with the new way configuration is managed.

--- src/cli/runCli.js
+++ src/cli/runCli.js
@@ -4,7 +4,7 @@ import { isString } from 'lodash';
 import execute from '../execute';
 import getAbsolutePath from '../helpers/getAbsolutePath';
 import validateSettingsWrapper from '../validation/validateSettingsWrapper';
-import { appendConfig } from '../configuration/manageConfig';
+import { setConfig } from '../configuration/manageConfig';
 import { appendSettings } from '../configuration/manageSettings';
 import merge from '../helpers/merge';
 import buildDocumentationObject from '../documentation/buildDocumentationObject';
@@ -53,7 +53,6 @@ export default function runCli({
     if (version || v) {
         return console.log(info.version);
     }
-
     // Possible to set a command in verbose mode
     const verboseMode = !!(verbose || V);
     setVerbose(verboseMode);
@@ -65,117 +64,118 @@ export default function runCli({
     const dirPath = getAbsolutePath(directory || d);
 
     // Initialize the complete context
-    return initContext({
+    const context = initContext({
         verbose: verboseMode,
         commands: initalCommands,
         directory: dirPath,
         projectConfigPath,
         name: info.name
-    }).then((context) => {
-        // If we have no command we will display some help information about all possible commands
-        if (!groupOrCommand) {
-            return console.log(
-                generateCommandsDocumentation(context.commands, info.name)
-            );
-        }
-
-        // Check if we are in a subgroup
-        const result = checkGroup(context.commands, groupOrCommand, args, info.name);
-        if (!result) {
-            return undefined;
-        }
-
-        let {
-            commands,
-            command,
-            parents
-        } = result;
-
-        let suggestions = Object.keys(commands);
-
-        // If there is no direct match we will search through the tree after a match
-        if (!commands[command]) {
-            const aliases = generateAliases(commands, command, parents);
-            if (!aliases) {
-                return undefined;
-            } else if (aliases.commands) {
-                commands = aliases.commands;
-                parents = aliases.parents;
-            }
-            suggestions = suggestions.concat(aliases.mappings);
-        }
-
-        if (!commands[command]) {
-            log.error(
-                getSuggestions([command], suggestions),
-                'Invalid command'
-            );
-        }
-
-        // Show command help information if requested
-        // Will ignore application configuration
-        if (help || h) {
-            return console.log(generateCommandDocumentation(context.extensionConfig.settings, context.meta.settings,
-                commands, command, info.name, parents));
-        }
-
-        const parsedArguments = parseArguments(command, commands, args);
-
-        let documentationObject;
-        // Only parse arguments if the command accepts it
-        if (commands[command] && commands[command].settings) {
-            // Get config from application and only parse options that this command cares about.
-            const filter = commands[command].settings === true ? [] : commands[command].settings;
-            documentationObject = buildDocumentationObject(context.config.settings, context.meta.settings, filter);
-        }
-
-        const { settings, parsedOptions } =
-            parseOptions(restOptions, getMappings(documentationObject), commands[command]);
-
-        const configToValidate = merge(context.config, {
-            settings
-        });
-
-        // Validate configuration
-        if (commands[command] && commands[command].settings) {
-            validateSettingsWrapper(configToValidate.settings, context.meta.settings, commands[command].settings);
-        }
-
-        // Does this after the validation so that things set by the CLI always will have the highest priority
-        context.config = merge(addRaw(context.config), {
-            settings
-        });
-
-        // Set the configuration object
-        appendConfig(context.config);
-
-        // Run hook to make it possible for extensions to update the settings before anything other uses them
-        runHook('roc')('update-settings', () => context.config.settings)(
-            (newSettings) => context.config.settings = appendSettings(newSettings, context.config)
-        );
-
-        if (invoke) {
-            // If string run as shell command
-            if (isString(commands[command].command)) {
-                return execute(commands[command].command)
-                    .catch(process.exit);
-            }
-
-            // Run the command
-            return commands[command].command({
-                verbose: verboseMode,
-                directory: dirPath || process.cwd(),
-                info,
-                parsedArguments,
-                parsedOptions,
-
-                // Roc Context
-                ...context,
-
-                // FIXME Temp
-                configObject: context.config,
-                metaObject: context.meta
-            });
-        }
     });
+
+    // If we have no command we will display some help information about all possible commands
+    if (!groupOrCommand) {
+        return console.log(
+            generateCommandsDocumentation(context.commands, info.name)
+        );
+    }
+
+    // Check if we are in a subgroup
+    const result = checkGroup(context.commands, groupOrCommand, args, info.name);
+    if (!result) {
+        return undefined;
+    }
+
+    let {
+        commands,
+        command,
+        parents
+    } = result;
+
+    let suggestions = Object.keys(commands);
+
+    // If there is no direct match we will search through the tree after a match
+    if (!commands[command]) {
+        const aliases = generateAliases(commands, command, parents);
+        if (!aliases) {
+            return undefined;
+        } else if (aliases.commands) {
+            commands = aliases.commands;
+            parents = aliases.parents;
+        }
+        suggestions = suggestions.concat(aliases.mappings);
+    }
+
+    if (!commands[command]) {
+        log.error(
+            getSuggestions([command], suggestions),
+            'Invalid command'
+        );
+    }
+
+    // Show command help information if requested
+    // Will ignore application configuration
+    if (help || h) {
+        return console.log(generateCommandDocumentation(context.extensionConfig.settings, context.meta.settings,
+            commands, command, info.name, parents));
+    }
+
+    const parsedArguments = parseArguments(command, commands, args);
+
+    let documentationObject;
+    // Only parse arguments if the command accepts it
+    if (commands[command] && commands[command].settings) {
+        // Get config from application and only parse options that this command cares about.
+        const filter = commands[command].settings === true ? [] : commands[command].settings;
+        documentationObject = buildDocumentationObject(context.config.settings, context.meta.settings, filter);
+    }
+
+    const { settings, parsedOptions } =
+        parseOptions(restOptions, getMappings(documentationObject), commands[command]);
+
+    const configToValidate = merge(context.config, {
+        settings
+    });
+
+    // Validate configuration
+    if (commands[command] && commands[command].settings) {
+        validateSettingsWrapper(configToValidate.settings, context.meta.settings, commands[command].settings);
+    }
+
+    // Does this after the validation so that things set by the CLI always will have the highest priority
+    context.config = merge(addRaw(context.config), {
+        settings
+    });
+
+    // Set the configuration object
+    setConfig(context.config);
+
+    // Run hook to make it possible for extensions to update the settings before anything other uses them
+    // This means that they can inspect what has been defined by other extensions, the user through config
+    // and through command line options
+    runHook('roc')('update-settings', () => context.config.settings)(
+        (newSettings) => context.config.settings = appendSettings(newSettings, context.config)
+    );
+
+    // Update the configuration object
+    setConfig(context.config);
+
+    if (invoke) {
+        // If string run as shell command
+        if (isString(commands[command].command)) {
+            return execute(commands[command].command)
+                .catch(process.exit);
+        }
+
+        // Run the command
+        return commands[command].command({
+            verbose: verboseMode,
+            directory: dirPath || process.cwd(),
+            info,
+            parsedArguments,
+            parsedOptions,
+
+            // Roc Context
+            context
+        });
+    }
 }
