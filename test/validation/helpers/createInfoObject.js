463d02f4f9ed49d7e7defaf66763b9d68835c44e
Andreas Søvik <andreas.sovik@vg.no>
2016-09-09 21:17:40 +0200

    Add test to validation createInfoObject
    Verifies that info provided by a passed validator is forwarded as
    expected

--- test/validation/helpers/createInfoObject.js
+++ test/validation/helpers/createInfoObject.js
@@ -13,6 +13,24 @@ describe('roc', () => {
                     converter: undefined,
                 });
             });
+
+            it('should create object with expected values when given a validator that provides info', () => {
+                const stubConverter = () => {};
+                const info = {
+                    type: 'testtype',
+                    canBeEmpty: true,
+                    required: true,
+                    converter: stubConverter,
+                };
+
+                const validatorProvidingInfo = () => info;
+
+                expect(
+                    createInfoObject({
+                        validator: validatorProvidingInfo,
+                    })
+                ).toEqual(info);
+            });
         });
     });
 });
