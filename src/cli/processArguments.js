0b17cebb7c320dccb969db5a7fe03a895819c783
Gustaf Dalemar <gustaf.dalemar@gmail.com>
2016-10-17 15:42:15 +0200

    CLI now use the provided --directory & fixed a problem with CLI errors

---
+++ src/cli/processArguments.js
@@ -0,0 +1,35 @@
+import minimist from 'minimist';
+
+export default function parseArguments(argv = process.argv) {
+    /* eslint-disable object-property-newline */
+    const {
+        _,
+        h, help,
+        V, verbose,
+        v, version,
+        c, config,
+        d, directory,
+        b, 'better-feedback': betterFeedback,
+        '--': extraArguments,
+        ...extOptions,
+    } = minimist(argv.slice(2), { '--': true });
+
+    // The first should be our command or commandgroup, if there is one
+    const [groupOrCommand, ...argsWithoutOptions] = _;
+
+    return {
+        groupOrCommand, // commandgroup or command
+        coreOptions: { // options managed and parsed by core
+            h, help,
+            V, verbose,
+            v, version,
+            c, config,
+            d, directory,
+            b, betterFeedback,
+        },
+        extOptions, // options that will be forwarded to commands from context
+        argsWithoutOptions, // remaining arguments with no associated options
+        extraArguments, // arguments after the ended argument list (--)
+    };
+    /* eslint-enable object-property-newline */
+}
